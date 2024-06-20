30cd9f8cfbc3863805a1dbfe393ed0825c58fefb
ishfaqmir <ishfaqmir@almuqeet.systems>
2024-06-20 12:30:55 +0530

    enabled cors middleware

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const taskRouter = require("./routes/tasks.routes");
 const managersRouter = require("./routes/managers.route");
 const { authMiddleWare } = require("./middlewares/auth-middleware");
 const app = express();
+const cors = require("cors");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
